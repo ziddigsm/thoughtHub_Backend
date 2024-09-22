488e77541c3c7739d2f67e07f9ab49d57e38cb07
ziddigsm <158247382+ziddigsm@users.noreply.github.com>
2024-09-21 20:35:51 -0400

    setup db connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,20 @@
 package main
 
-import "github.com/ziddigsm/thoughtHub_Backend/cmd/api"
+import (
+	"github.com/ziddigsm/thoughtHub_Backend/cmd/api"
+	"github.com/ziddigsm/thoughtHub_Backend/db"
+)
 
 func main() {
+
+	db, err := db.DbConnection()
+	if err != nil {
+		panic("Error initializing database")
+	}
+	defer db.Close()
 	server := api.Server("localhost:8080", nil)
 	if err := server.Run(); err != nil {
 		panic(err)
 	}
+
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/ziddigsm/thoughtHub_Backend
 go 1.23.1
 
 require github.com/gorilla/mux v1.8.1
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9 
+)
