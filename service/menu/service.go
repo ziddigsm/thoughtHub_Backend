99ba6f69ec245f63b66695422d41c47dd7730067
ziddigsm <158247382+ziddigsm@users.noreply.github.com>
2024-09-27 17:58:54 -0400

    setup menu api to fetch the list of menu/settings options

--- service/menu/service.go
+++ service/menu/service.go
@@ -1,1 +1,22 @@
 package menu
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/ziddigsm/thoughtHub_Backend/types"
+	"github.com/ziddigsm/thoughtHub_Backend/utils"
+)
+
+func (h *Handler) GetMenu(w http.ResponseWriter, r *http.Request) {
+	var menu []types.Menu
+
+	query := r.URL.Query()
+	isNavbar, _ := strconv.ParseBool(query.Get("is_navbar"))
+	if err := h.db.Where("is_navbar = ? and is_active = ?", isNavbar, true).Find(&menu).Error; err != nil {
+		utils.ErrorResponse(w, http.StatusInternalServerError, fmt.Errorf("failed to fetch menu: %v", err))
+		return
+	}
+	utils.SuccessResponse(w, http.StatusOK, menu)
+}
--- types/types.go
+++ types/types.go
@@ -6,12 +6,21 @@ import (
 
 )
 
-type User struct {
+type Users struct {
     ID        int            `gorm:"primaryKey;autoIncrement" json:"-"`
     Mail      string         `gorm:"size:255;unique;not null" json:"mail"`
     Name      string         `gorm:"size:100;not null" json:"name"`
     Username  string         `gorm:"size:20" json:"username,omitempty"`
     IsActive  bool           `gorm:"default:true" json:"-"`
     CreatedOn time.Time `gorm:"autoCreateTime" json:"-"`
+    UpdatedOn time.Time `gorm:"autoUpdateTime" json:"-"`
+}
+
+type Menu struct {
+	ID int `gorm:"primaryKey;autoIncrement" json:"id"`
+	Options string `gorm:"size:50" json:"option"`
+	Is_active bool `gorm:"default:true" json:"is_active"`
+	Is_navbar bool `gorm:"default:true" json:"is_navbar"`
+	CreatedOn time.Time `gorm:"autoCreateTime" json:"-"`
     UpdatedOn time.Time `gorm:"autoUpdateTime" json:"-"`
 }
